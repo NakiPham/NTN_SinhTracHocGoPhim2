ad0e1674495a9c5a4ee4237b4f634c7d96b5062e
NhiPham <mi4.force.work@gmail.com>
2017-11-25 11:02:50 +0700

    UPLOAD

---
+++ app/src/main/java/com/example/keystrokedynamics/DatabaseNetwork.java
@@ -0,0 +1,10 @@
+package com.example.keystrokedynamics;
+
+/**
+ * Created by hp on 25/11/2017.
+ */
+
+public class DatabaseNetwork {
+    public static final
+
+}
---
+++ app/src/main/java/com/example/keystrokedynamics/MainActivity.java
@@ -0,0 +1,9 @@
+package com.example.keystrokedynamics;
+
+/**
+ * Created by hp on 25/11/2017.
+ */
+
+public class MainActivity {
+
+}
